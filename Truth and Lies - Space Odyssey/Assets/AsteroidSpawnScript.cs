0fd33db77568426e5fe8324afb61aa19197a1ede
Rbascoe <roanbascoe5@gmail.com>
2023-05-03 20:50:51 -0400

    added timer and code for ensuring the asteroids stop spawning

--- Truth and Lies - Space Odyssey/Assets/AsteroidSpawnScript.cs
+++ Truth and Lies - Space Odyssey/Assets/AsteroidSpawnScript.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class AsteroidSpawnScript : MonoBehaviour
 {
+
+    public LogicScript gameTimer;
     public GameObject asteroid;
 
     public float spawnRate = 15f;
     private float timer = 0;
+
+    public bool startSpawn;
     // Start is called before the first frame update
     void Start()
     {
+        gameTimer = GameObject.FindGameObjectWithTag("Timer").GetComponent<LogicScript>();
+        startSpawn = true;
         spawnAsteroid();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         if (timer < spawnRate)
         {
             timer += Time.deltaTime;
@@ -25,13 +33,38 @@ public class AsteroidSpawnScript : MonoBehaviour
         {
             spawnAsteroid();
             timer = 0;
+            // stopSpawn = false;
         }
+
+
+        // if (timer < spawnRate)
+        // {
+        //     timer += Time.deltaTime;
+        // }
+        // else
+        // {
+        //     spawnAsteroid();
+        //     timer = 0;
+
+        // }
+
+
     }
 
     void spawnAsteroid()
     {
-
-        Instantiate(asteroid, transform.position, transform.rotation);
-    
+        if (startSpawn)
+        {
+            Debug.Log(gameTimer.gameTimer());
+            if (gameTimer.gameTimer() != "0")
+            {
+                Instantiate(asteroid, transform.position, transform.rotation);
+            }
+            else
+            {
+                startSpawn = false;
+                Debug.Log("Spawn Stopped");
+            }
+        }
     }
 }
--- Truth and Lies - Space Odyssey/Assets/LogicScript.cs
+++ Truth and Lies - Space Odyssey/Assets/LogicScript.cs
@@ -1,18 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LogicScript : MonoBehaviour
 {
+
+    public TextMeshProUGUI timerText;
+    //Timer Settings
+    public float timeRemaining;
+    public bool timerIsRunning = false;
+
+    //public bool hasLimit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeRemaining = 35f;
+        timerIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       gameTimer();
     }
+
+    // public float timer()
+    // {
+        // if (timerIsRunning)
+        // {
+        //     if (timeRemaining > 0)
+        //     {
+        //         timeRemaining -= Time.deltaTime;
+        //         // Debug.Log(timeRemaining);
+        //         if (timeRemaining == 0)
+        //         {
+        //             Debug.Log("Time has run out!");
+        //             timerIsRunning = false;
+        //             return timeRemaining;
+        //         }
+        //     }
+            // else
+            // {
+            //     Debug.Log("Time has run out!");
+            //     timeRemaining = 0;
+            //     timerIsRunning = false;
+            //     return timeRemaining;
+            // }
+        // }
+        // Debug.Log(timerText.text = timeRemaining.ToString());
+        // return timeRemaining;
+    //     for (float i = timeRemaining; i > 0; i = i - Time.deltaTime){
+    //         timeRemaining = i;
+    //     }
+    //     return timeRemaining;
+    // }
+
+    public string gameTimer()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                // Debug.Log(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+            return timerText.text = timeRemaining.ToString();
+    }
+
 }
